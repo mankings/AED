4cfcfc2f382dd505c2f5cb155ae3c076a7be42b9
mankings <miguelamatos02@gmail.com>
2021-12-09 15:25:24 +0000

    Stack

--- aula09/stack/aStack.h
+++ aula09/stack/aStack.h
@@ -9,55 +9,44 @@
 
 #include <cassert>
 
-template <typename T>
-class aStack
-{
+template <typename T> class aStack {
+  
   private:
     int max_size; // maximum stack size
     int cur_size; // current stack size
     T *data;      // the stack data (stored in an array)
+
   public:
-    aStack(int n = 100)
-    {
-      assert(n >= 10 && n <= 1000000);
-      max_size = n;
-      cur_size = 0;
-      data = new T[n];
+    aStack(int n = 100) {
+        assert(n >= 10 && n <= 1000000);
+        max_size = n;
+        cur_size = 0;
+        data = new T[n];
     }
-    ~aStack(void)
-    {
-      delete[] data;
+
+    ~aStack(void) { delete[] data; }
+
+    void clear(void) { cur_size = 0; }
+
+    int size(void) const { return cur_size; }
+
+    int is_full(void) const { return (cur_size == max_size) ? 1 : 0; }
+
+    int is_empty(void) const { return (cur_size == 0) ? 1 : 0; }
+
+    T top(void) const {
+        assert(cur_size > 0);
+        return data[cur_size - 1];
     }
-    void clear(void)
-    {
-      cur_size = 0;
+
+    void push(T &v) {
+        assert(cur_size < max_size);
+        data[cur_size++] = v;
     }
-    int size(void) const
-    {
-      return cur_size;
-    }
-    int is_full(void) const
-    {
-      return (cur_size == max_size) ? 1 : 0;
-    }
-    int is_empty(void) const
-    {
-      return (cur_size == 0) ? 1 : 0;
-    }
-    T top(void) const
-    {
-      assert(cur_size > 0);
-      return data[cur_size - 1];
-    }
-    void push(T &v)
-    {
-      assert(cur_size < max_size);
-      data[cur_size++] = v;
-    }
-    T pop(void)
-    {
-      assert(cur_size > 0);
-      return data[--cur_size];
+
+    T pop(void) {
+        assert(cur_size > 0);
+        return data[--cur_size];
     }
 };
 
--- aula09/stack/aStack_demo.cpp
+++ aula09/stack/aStack_demo.cpp
@@ -4,34 +4,45 @@
 // matched-parenthesis verification
 //
 
-#include <iostream>
 #include "aStack.h"
+#include <iostream>
 
-using std::cout;
 using std::cerr;
+using std::cout;
 using std::endl;
 
-int check_parenthesis(const char *s)
-{
-  // put your code here (20 lines of code should be enough)
-  return -1;
+int check_parenthesis(const char *s) {
+    aStack<int> intStack;
+    for (int i = 0; s[i] != '\0'; i++) {
+
+      if(s[i] == '(') { intStack.push(i); }
+      else if(s[i] == ')') { 
+        if(intStack.is_empty()) {
+          cout << "  unmatched ')' at position " << i+1 << endl;
+          return -1;
+        }
+        cout << "  '(' at position " << intStack.pop()+1 << " and matching ')' at position " << i+1 << endl;
+      }
+    }
+
+    if(intStack.is_empty()) { return 0; }
+    cout << "  unmatched '(' at position " << intStack.pop()+1 << endl;
+    return -1;
 }
 
-int main(int argc,char **argv)
-{
-  if(argc == 1)
-  {
-    cerr << "usage: " << argv[0] << " [arguments...]" << endl;
-    cerr << "example: " << argv[0] << " 'abc(def)ghi' 'x)(y'" << endl;
-    return 1;
-  }
-  for(int i = 1;i < argc;i++)
-  {
-    cout << argv[i] << endl;
-    if(check_parenthesis(argv[i]) == 0)
-      cout << "  good" << endl;
-    else
-      cout << "  bad" << endl;
-  }
-  return 0;
+int main(int argc, char **argv) {
+    if (argc == 1) {
+        cerr << "usage: " << argv[0] << " [arguments...]" << endl;
+        cerr << "example: " << argv[0] << " 'abc(def)ghi' 'x)(y'" << endl;
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        cout << argv[i] << endl;
+        if (check_parenthesis(argv[i]) == 0)
+            cout << "  good" << endl;
+        else
+            cout << "  bad" << endl;
+    }
+    return 0;
 }
